84ed7608bb4f5af73a99ea6c44964239cbada3c2
Gian <me@gripep.com>
2023-01-21 12:32:10 +0000

    Remove unnecessary ecxeption

--- drf_problem_detail_exceptions/exceptions.py
+++ drf_problem_detail_exceptions/exceptions.py
@@ -3,11 +3,6 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
-class RFC7807Error(APIException):
-    def __init__(self):
-        raise NotImplementedError()
-
-
 class ServerError(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = _("Server error.")
